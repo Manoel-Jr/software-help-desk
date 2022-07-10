6c3691dd45887d8f564d5756aadb0c3f7a400daf
Junior Healy <juniorhealy2010@hotmail.com>
2022-07-10 11:21:23 -0300

    criado as classes de deminio e add anotações jpa

--- src/main/java/com/example/software/entity/Cliente.java
+++ src/main/java/com/example/software/entity/Cliente.java
@@ -1,5 +1,42 @@
 package com.example.software.entity;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.example.software.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha, List<Chamado> chamados) {
+		super(id, nome, cpf, email, senha);
+		this.chamados = chamados;
+	}
 
 }
